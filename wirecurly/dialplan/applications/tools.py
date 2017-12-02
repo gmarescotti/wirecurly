1009c518db794abc1389f6f4ad0286fa736b4a6b
gmarescotti <giulio.marescotti@fastwebnet.it>
2017-12-02 10:32:27 +0100

    mixed indentation warning
    - Converted space tabs into tabs

--- wirecurly/dialplan/applications/tools.py
+++ wirecurly/dialplan/applications/tools.py
@@ -120,18 +120,18 @@ class Export(ApplicationBase):
 			return '%s=%s' % (self.variable, self.value)
 
 class ExecuteOnAnswer(ApplicationBase):
-    """ExecuteOnAnswer a variable on the other b leg"""
-    def __init__(self, cmd, nolocal=True):
-        super(ExecuteOnAnswer, self).__init__('export')
-        self.cmd = cmd
-        self.nolocal = nolocal
+	"""ExecuteOnAnswer a variable on the other b leg"""
+	def __init__(self, cmd, nolocal=True):
+		super(ExecuteOnAnswer, self).__init__('export')
+		self.cmd = cmd
+		self.nolocal = nolocal
 
-    @property
-    def data(self):
-        '''
-            Set needs return a string
-        '''
-        if self.nolocal:
-            return 'nolocal:execute_on_answer=%s' % self.cmd
-        else:
-            return 'execute_on_answer=%s' % self.cmd
+	@property
+	def data(self):
+		'''
+			Set needs return a string
+		'''
+		if self.nolocal:
+			return 'nolocal:execute_on_answer=%s' % self.cmd
+		else:
+			return 'execute_on_answer=%s' % self.cmd
