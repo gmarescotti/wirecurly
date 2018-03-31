a1a48ca850a6e14695d29d7b5c5a37abcd56b934
gmarescotti <giulio.marescotti@fastwebnet.it>
2018-03-31 15:52:59 +0200

    Added bridged type of voicemail

--- wirecurly/dialplan/applications/Voicemail.py
+++ wirecurly/dialplan/applications/Voicemail.py
@@ -2,8 +2,12 @@ from wirecurly.dialplan.applications import ApplicationBase
 
 class Voicemail(ApplicationBase):
 	"""Voicemail application"""
-	def __init__(self, datastring):
-		super(Voicemail, self).__init__('voicemail')
+	def __init__(self, datastring, use_bridge):
+		self.use_bridge = use_bridge
+		if use_bridge:
+			super(Voicemail, self).__init__('voicemail')
+		else:
+			super(Voicemail, self).__init__('bridge')
 		self.datastring = datastring
 
 	@property
@@ -11,5 +15,8 @@ class Voicemail(ApplicationBase):
 		'''
 			Data is the whole datastring
 		'''
+		if self.use_bridge:
+			return "loopback/app=voicemail: %s" % self.datastring
+
 		return self.datastring
 	
