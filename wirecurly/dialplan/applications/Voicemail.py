0b9258466f9487c1bbdd6be6b48934b8f392122c
gmarescotti <giulio.marescotti@fastwebnet.it>
2018-03-31 15:55:43 +0200

    Defaulted to false bridged type of voicemail

--- wirecurly/dialplan/applications/Voicemail.py
+++ wirecurly/dialplan/applications/Voicemail.py
@@ -2,7 +2,7 @@ from wirecurly.dialplan.applications import ApplicationBase
 
 class Voicemail(ApplicationBase):
 	"""Voicemail application"""
-	def __init__(self, datastring, use_bridge):
+	def __init__(self, datastring, use_bridge=False):
 		self.use_bridge = use_bridge
 		if use_bridge:
 			super(Voicemail, self).__init__('voicemail')
